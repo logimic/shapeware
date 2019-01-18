2fb14f37e114207f1bacc362bd672a5f390046e6
Frantisek Mikulu <frantisek.mikulu@logimic.com>
2019-01-18 19:36:41 +0100

    First usage of wss

--- WebsocketCppService/WebsocketCppService.cpp
+++ WebsocketCppService/WebsocketCppService.cpp
@@ -26,6 +26,7 @@
 #define _WEBSOCKETPP_CPP11_INTERNAL_
 
 #include <websocketpp/config/asio_no_tls.hpp>
+#include <websocketpp/config/asio.hpp>
 #include <websocketpp/server.hpp>
 
 #include "shape__WebsocketCppService.hxx"
@@ -40,7 +41,12 @@ TRC_INIT_MODULE(shape::WebsocketCppService);
 namespace shape {
 
   typedef websocketpp::connection_hdl connection_hdl;
-  typedef websocketpp::server<websocketpp::config::asio> WsServer;
+  //typedef websocketpp::server<websocketpp::config::asio> WsServer;
+  typedef websocketpp::server<websocketpp::config::asio_tls> WsServer;
+
+  // pull out the type of messages sent by our config
+  typedef websocketpp::config::asio::message_type::ptr message_ptr;
+  typedef websocketpp::lib::shared_ptr<websocketpp::lib::asio::ssl::context> context_ptr;
 
   class LogStream : public std::streambuf {
   private:
@@ -104,7 +110,7 @@ namespace shape {
     void on_message(connection_hdl hdl, WsServer::message_ptr msg)
     {
       TRC_FUNCTION_ENTER("");
-      
+
       std::string connId;
       bool found = false;
       {
@@ -143,8 +149,9 @@ namespace shape {
       //TODO on_connection can be use instead, however we're ready for authentication by a token
       TRC_FUNCTION_ENTER("");
       bool retval = true;
-      
-      websocketpp::server<websocketpp::config::asio>::connection_ptr con = m_server.get_con_from_hdl(hdl);
+
+      //websocketpp::server<websocketpp::config::asio>::connection_ptr con = m_server.get_con_from_hdl(hdl);
+      WsServer::connection_ptr con = m_server.get_con_from_hdl(hdl);
 
       //TODO provision id
       std::ostringstream os;
@@ -183,9 +190,10 @@ namespace shape {
     void on_fail(connection_hdl hdl)
     {
       TRC_FUNCTION_ENTER("");
-      websocketpp::server<websocketpp::config::asio>::connection_ptr con = m_server.get_con_from_hdl(hdl);
+      //websocketpp::server<websocketpp::config::asio>::connection_ptr con = m_server.get_con_from_hdl(hdl);
+      WsServer::connection_ptr con = m_server.get_con_from_hdl(hdl);
       websocketpp::lib::error_code ec = con->get_ec();
-      TRC_WARNING("Error: " << NAME_PAR(hdl, hdl.lock().get()) << " " << ec.message() );
+      TRC_WARNING("Error: " << NAME_PAR(hdl, hdl.lock().get()) << " " << ec.message());
       TRC_FUNCTION_LEAVE("");
     }
 
@@ -206,7 +214,7 @@ namespace shape {
       websocketpp::lib::error_code ec;
       m_server.close(hdl, websocketpp::close::status::normal, data, ec); // send close message.
       if (ec) { // we got an error
-        TRC_WARNING("Error: " << PAR(connId) << " " << ec.message() );
+        TRC_WARNING("Error: " << PAR(connId) << " " << ec.message());
       }
 
       TRC_FUNCTION_LEAVE("");
@@ -218,7 +226,7 @@ namespace shape {
       std::string connId;
       bool found = false;
       {
-    	std::unique_lock<std::mutex> lock(m_mux);
+        std::unique_lock<std::mutex> lock(m_mux);
         found = getConnId(hdl, connId);
         m_connectionsStrMap.erase(hdl);
       }
@@ -234,6 +242,77 @@ namespace shape {
         }
       }
       TRC_FUNCTION_LEAVE("");
+    }
+    ///////////////////////////////
+    //?????????????????????????????
+    void on_http(connection_hdl hdl) {
+      WsServer::connection_ptr con = m_server.get_con_from_hdl(hdl);
+
+      con->set_body("Hello World!");
+      con->set_status(websocketpp::http::status_code::ok);
+    }
+
+    std::string get_password() {
+      return "test";
+    }
+
+    // See https://wiki.mozilla.org/Security/Server_Side_TLS for more details about
+    // the TLS modes. The code below demonstrates how to implement both the modern
+    enum tls_mode {
+      MOZILLA_INTERMEDIATE = 1,
+      MOZILLA_MODERN = 2
+    };
+
+    context_ptr on_tls_init(tls_mode mode, connection_hdl hdl) {
+      namespace asio = websocketpp::lib::asio;
+
+      std::cout << "on_tls_init called with hdl: " << hdl.lock().get() << std::endl;
+      std::cout << "using TLS mode: " << (mode == MOZILLA_MODERN ? "Mozilla Modern" : "Mozilla Intermediate") << std::endl;
+
+      context_ptr ctx = websocketpp::lib::make_shared<asio::ssl::context>(asio::ssl::context::sslv23);
+
+      try {
+        if (mode == MOZILLA_MODERN) {
+          // Modern disables TLSv1
+          ctx->set_options(asio::ssl::context::default_workarounds |
+            asio::ssl::context::no_sslv2 |
+            asio::ssl::context::no_sslv3 |
+            asio::ssl::context::no_tlsv1 |
+            asio::ssl::context::single_dh_use);
+        }
+        else {
+          ctx->set_options(asio::ssl::context::default_workarounds |
+            asio::ssl::context::no_sslv2 |
+            asio::ssl::context::no_sslv3 |
+            asio::ssl::context::single_dh_use);
+        }
+        //ctx->set_password_callback(bind(&get_password));
+        ctx->use_certificate_chain_file("cert.pem");
+        ctx->use_private_key_file("key.pem", asio::ssl::context::pem);
+
+        // Example method of generating this file:
+        // `openssl dhparam -out dh.pem 2048`
+        // Mozilla Intermediate suggests 1024 as the minimum size to use
+        // Mozilla Modern suggests 2048 as the minimum size to use.
+        //ctx->use_tmp_dh_file("dh.pem");
+
+        std::string ciphers;
+
+        if (mode == MOZILLA_MODERN) {
+          ciphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!3DES:!MD5:!PSK";
+        }
+        else {
+          ciphers = "ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256:DHE-DSS-AES128-GCM-SHA256:kEDH+AESGCM:ECDHE-RSA-AES128-SHA256:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA:ECDHE-ECDSA-AES128-SHA:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA:ECDHE-ECDSA-AES256-SHA:DHE-RSA-AES128-SHA256:DHE-RSA-AES128-SHA:DHE-DSS-AES128-SHA256:DHE-RSA-AES256-SHA256:DHE-DSS-AES256-SHA:DHE-RSA-AES256-SHA:AES128-GCM-SHA256:AES256-GCM-SHA384:AES128-SHA256:AES256-SHA256:AES128-SHA:AES256-SHA:AES:CAMELLIA:DES-CBC3-SHA:!aNULL:!eNULL:!EXPORT:!DES:!RC4:!MD5:!PSK:!aECDH:!EDH-DSS-DES-CBC3-SHA:!EDH-RSA-DES-CBC3-SHA:!KRB5-DES-CBC3-SHA";
+        }
+
+        if (SSL_CTX_set_cipher_list(ctx->native_handle(), ciphers.c_str()) != 1) {
+          std::cout << "Error setting cipher list" << std::endl;
+        }
+      }
+      catch (std::exception& e) {
+        std::cout << "Exception: " << e.what() << std::endl;
+      }
+      return ctx;
     }
     ///////////////////////////////
 
@@ -461,10 +540,20 @@ namespace shape {
         on_close(hdl);
       });
 
-      m_server.set_message_handler([&](connection_hdl hdl, WsServer::message_ptr msg) {
-        on_message(hdl, msg);
+      //m_server.set_http_handler([&](connection_hdl hdl, WsServer::message_ptr msg) {
+      //  on_message(hdl, msg);
+      //});
+
+//??????????????
+      m_server.set_http_handler([&](connection_hdl hdl) {
+        on_http(hdl);
       });
 
+      m_server.set_tls_init_handler([&](connection_hdl hdl)->context_ptr {
+        return on_tls_init(MOZILLA_INTERMEDIATE, hdl);
+      });
+
+//??????????????
       if (m_autoStart) {
         start();
       }
