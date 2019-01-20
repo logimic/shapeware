fd69df842ed4330f783cd5a863e4384e08d1af1e
Michal Valny <michal.valny@logimic.com>
2019-01-20 16:32:38 +0100

    python building script

---
+++ build.py
@@ -0,0 +1,74 @@
+# 
+# Copyright 2018 Logimic,s.r.o.
+# www.logimic.com
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+import sys, os
+import pathlib
+import shutil
+import argparse
+from subprocess import call
+
+mainDir  = ""
+
+def main():
+
+    parser = argparse.ArgumentParser(description='Building script for Shape.')
+    parser.add_argument('-G','--gen', help='Generator', required=False)
+    # parser.add_argument('-D', action='append', nargs='*', help='Description for bar argument', required=False)    
+    args = parser.parse_args()
+
+    if args.gen:
+        print("Generator:" + args.gen)    
+
+    buildexp=os.path.normpath("/build/VS15_2017_x64") 
+    buildDir = os.path.normpath(mainDir + buildexp) 
+    
+    # Create build dir
+    if not os.path.exists(buildDir):
+        os.makedirs(buildDir)   
+
+    generator = ""
+    if args.gen:
+        generator = " -G \"" + args.gen +  "\""   
+
+    os.chdir("..")
+    shape = os.path.normpath(os.getcwd() + "/shape/" +  buildexp)
+
+    vcpkg=os.path.normpath("c:/devel/vcpkg/scripts/buildsystems/vcpkg.cmake")
+    dTesting = "-DBUILD_TESTING:BOOL=true"    
+    dShape= "-Dshape_DIR:PATH=" + shape
+    dSsl = "-DLWS_WITH_SSL:BOOL=false"
+    dToolChain = "-DCMAKE_TOOLCHAIN_FILE=" + vcpkg
+        
+    parameters = " " + dTesting + " " + dToolChain + " " + dSsl + " " + dShape
+
+    command = "cmake" + generator + parameters + " " + mainDir        
+
+    #cmake -G "Visual Studio 15 2017 Win64" -DBUILD_TESTING:BOOL=true -DCMAKE_TOOLCHAIN_FILE=%vcpkg% -DLWS_WITH_SSL:BOOL=false -Dshape_DIR:PATH=%shape% %currentdir%
+    print("command: " + command)  
+    os.chdir(buildDir)   
+    call(command)    
+
+    os.chdir(mainDir)
+    command = "cmake --build " + buildDir + " --config Debug --target install"
+    call(command)  
+
+    command = "cmake --build " + buildDir + " --config Release --target install"
+    call(command)      
+
+if __name__ == "__main__":
+    mainDir  = os.getcwd()
+    main()
