ca9115a84d768b5b9582da81811d8310d688828e
Michal Valny <michal.valny@logimic.com>
2019-02-25 11:35:45 +0100

    building scripts

--- build.py
+++ build.py
@@ -19,65 +19,140 @@ import sys, os
 import pathlib
 import shutil
 import argparse
+import json
+import platform
 from subprocess import call
+
+# Date: 25-Feb-2018
+# Version:
+ver = '1.0.0'
 
 mainDir  = ""
 
 def main():
 
     parser = argparse.ArgumentParser(description='Building script for Shape.')
-    parser.add_argument('-G','--gen', help='Generator. If you do not know call command \"cmake -G\"', required=False)
+    # parser.add_argument('-C','--json', help='Configuration JSON file', required=False)
+    # parser.add_argument('-G','--gen', help='Generator. If you do not know call command \"cmake -G\"', required=False)
     # parser.add_argument('-D', action='append', nargs='*', help='Description for bar argument', required=False)    
     args = parser.parse_args()
 
-    if not args.gen:
-        print("Generator not specified, try use: -h")
+    # Welcome print
+    print('*****************************************************')
+    print('Building procedure by Logimic, s.r.o, www.logimic.com')
+    print('Version:' + ver)
+    print('*****************************************************')
+
+    # Determine config JSON file
+    buildCfg = ''
+    system = platform.system()
+
+    if system == 'Windows':
+        buildCfg = 'bcfgWin.json'
+    elif system == 'Linux':
+        buildCfg = 'bcfgLin.json'
+    else:
         return None
 
+    print('Platform file: ' + buildCfg)
+
+    # Open config file
+    try:
+        with open(buildCfg) as f:
+            data = json.load(f) 
+    except IOError:
+        print("Wrong path or config JSON file.!!!")
+        return None      
+
+    # Check keys
+    if ('generator' not in data):
+        print("\generator\" key is not specified in JSON file.")
+        return None
+
+    if ('parameters' not in data):
+        print("\parameters\" key is not specified in JSON file.")
+        return None        
+
+    # Set parametres
+    gen = data["generator"]
+    params = data["parameters"]
+    
     buildFolder = "Unknown_Builder"
 
-    if args.gen:
-        print("Generator:" + args.gen)
-        if args.gen == "Unix Makefiles":
+    if gen:
+        print("Generator:" + gen)
+        if gen == "Unix Makefiles":
             buildFolder = "Unix_Makefiles"
-        elif args.gen == "Visual Studio 15 2017 Win64":
+        elif gen == "Visual Studio 15 2017 Win64":
             buildFolder = "VS15_2017_x64"  
-        elif args.gen == "Visual Studio 15 2017":
-            buildFolder = "VS15_2017"       
+        elif gen == "Visual Studio 15 2017":            
+            buildFolder = "VS15_2017"                      
 
     buildDir = os.path.normpath(mainDir + "/build/" + buildFolder) 
     
     # Create build dir
     if not os.path.exists(buildDir):
-        os.makedirs(buildDir)   
+        os.makedirs(buildDir)  
 
-    generator = " -G \"" + args.gen +  "\""     
+    # Generator
+    generator = " -G \"" + gen + "\""
 
-    # Shape
-    os.chdir("..")
-    shape = os.path.normpath(os.getcwd() + "/shape/" +  "/build/" + buildFolder)
+    # Parameters      
+    dParams = ""
 
-    vcpkg = ""
-    if args.gen == "Unix Makefiles":
-        vcpkg = ""
-    elif args.gen == "Visual Studio 15 2017 Win64":
-        vcpkg = os.path.normpath("c:/devel/vcpkg/scripts/buildsystems/vcpkg.cmake")    
-    elif args.gen == "Visual Studio 15 2017":
-        vcpkg = os.path.normpath("c:/devel/vcpkg/scripts/buildsystems/vcpkg.cmake")            
-    else:
-        vcpkg = ""
+    for param in params:
+        print(param) 
+        if "SHAPE_DEPLOY:PATH=" in param:
+            index = param.index('=')
+            dDir = param[index + 1:]       
+ 
+            if dDir == './' or dDir == '.' or dDir == '' or dDir == ' ': # Shape internal deploy                
+                deployDir = os.path.normpath(mainDir + "/deploy/" + buildFolder)
+            else:
+                deployDir = os.path.normpath(dDir + '/' + buildFolder)
+            
+            if not os.path.exists(deployDir):
+                os.makedirs(deployDir)
 
-    dTesting = "-DBUILD_TESTING:BOOL=true"    
-    dShape= "-Dshape_DIR:PATH=" + shape
-    dSsl = "-DLWS_WITH_SSL:BOOL=false"
-    dToolChain = "-DCMAKE_TOOLCHAIN_FILE=" + vcpkg
-    dDebug = "-DCMAKE_BUILD_TYPE=Debug"
-    dRelease = "-DCMAKE_BUILD_TYPE=Release"
+            param = 'SHAPE_DEPLOY:PATH=' + deployDir
+
+        if "shape_DIR:PATH=" in param:
+            index = param.index('=')
+            dDir = param[index + 1:]       
+ 
+            if dDir == './' or dDir == '.' or dDir == '' or dDir == ' ': # Shape internal deploy                                
+                os.chdir("..")
+                shapeDir = os.path.normpath(os.getcwd() + "/shape/" +  "/build/" + buildFolder)      
+                os.chdir(mainDir)          
+            else:
+                shapeDir = os.path.normpath(dDir + '/' + buildFolder)
+
+            param = 'shape_DIR:PATH=' + shapeDir
+
+        if "shapeware_DIR:PATH=" in param:
+            index = param.index('=')
+            dDir = param[index + 1:]       
+ 
+            if dDir == './' or dDir == '.' or dDir == '' or dDir == ' ': # Shape internal deploy                                
+                os.chdir("..")
+                shapewareDir = os.path.normpath(os.getcwd() + "/shapeware/" +  "/build/" + buildFolder)      
+                os.chdir(mainDir)          
+            else:
+                shapewareDir = os.path.normpath(dDir + '/' + buildFolder)
+
+            param = 'shapeware_DIR:PATH=' + shapewareDir            
+            
+        dParams = dParams + " -D" + param
+
+    print('Final params: ' + dParams)  
+
+    #return None     
 
     # Building
-    if args.gen == "Unix Makefiles":
-        command = "cmake" + generator + " " + dTesting + " " + dDebug + " " + dToolChain + " " + dSsl + " " + dShape + " " + mainDir        
-        
+    if gen == "Unix Makefiles":
+ 
+        command = "cmake" + generator + " " + dParams + " -DCMAKE_BUILD_TYPE=Debug " + mainDir        
+
         print("command: " + command)  
         os.chdir(buildDir)   
         os.system(command)    
@@ -86,19 +161,18 @@ def main():
         command = "cmake --build " + buildDir + " --config Debug --target install"
         os.system(command)  
 
-        command = "cmake" + generator + " " + dTesting + " " + dRelease + " " + dToolChain + " " + dSsl + " " + dShape + " " + mainDir        
-        
+        command = "cmake" + generator + " " + dParams + " -DCMAKE_BUILD_TYPE=Release " + mainDir          
+
         print("command: " + command)  
         os.chdir(buildDir)   
-        os.system(command)            
+        os.system(command)          
 
-        os.chdir(mainDir)
         command = "cmake --build " + buildDir + " --config Release --target install"
         os.system(command)     
 
-    else: 
-        command = "cmake" + generator + " " + dTesting + " " + dToolChain + " " + dSsl + " " + dShape + " " + mainDir        
-        
+    else:
+        command = "cmake" + generator + " " + dParams + " " + mainDir        
+
         print("command: " + command)  
         os.chdir(buildDir)   
         os.system(command)    
@@ -108,7 +182,8 @@ def main():
         os.system(command)  
 
         command = "cmake --build " + buildDir + " --config Release --target install"
-        os.system(command)             
+        os.system(command)
+
 
 if __name__ == "__main__":
     mainDir  = os.getcwd()
