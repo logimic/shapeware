2e399f250a619b976dd6a2f8e9ca784f2abb8522
Frantisek Mikulu <frantisek.mikulu@logimic.com>
2019-10-02 21:12:02 +0200

    Add JsonMacro.h for general use

---
+++ include/JsonMacro.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#define GET_JSON_AS_NUM(jsonVal, pointerPath, value) \
+{ \
+  const rapidjson::Value *pv = rapidjson::Pointer(pointerPath).Get(jsonVal); \
+  if (!(pv && pv->IsNumber())) { \
+    THROW_EXC_TRC_WAR(std::logic_error, "Missing or bad type: \"" << pointerPath << "\"") \
+  } \
+  value = pv->GetDouble(); \
+}
+
+#define GET_JSON_AS_INT(jsonVal, pointerPath, value) \
+{ \
+  const rapidjson::Value *pv = rapidjson::Pointer(pointerPath).Get(jsonVal); \
+  if (!(pv && pv->IsNumber())) { \
+    THROW_EXC_TRC_WAR(std::logic_error, "Missing or bad type: \"" << pointerPath << "\"") \
+  } \
+  value = static_cast<int>(pv->GetDouble()); \
+}
+
+#define GET_JSON_AS_BOOL(jsonVal, pointerPath, value) \
+{ \
+  const rapidjson::Value *pv = rapidjson::Pointer(pointerPath).Get(jsonVal); \
+  if (!(pv && pv->IsBool())) { \
+    THROW_EXC_TRC_WAR(std::logic_error, "Missing or bad type: \"" << pointerPath << "\"") \
+  } \
+  value = pv->GetBool(); \
+}
+
+#define GET_JSON_AS_STR(jsonVal, pointerPath, value) \
+{ \
+  const rapidjson::Value *pv = rapidjson::Pointer(pointerPath).Get(jsonVal); \
+  if (!(pv && pv->IsString())) { \
+    THROW_EXC_TRC_WAR(std::logic_error, "Missing or bad type: \"" << pointerPath << "\"") \
+  } \
+  value = pv->GetString(); \
+}
+
+#define GET_JSON_AS_OBJECT(jsonVal, pointerPath, value) \
+{ \
+  const rapidjson::Value *pv = rapidjson::Pointer(pointerPath).Get(jsonVal); \
+  if (!(pv && pv->IsObject())) { \
+    THROW_EXC_TRC_WAR(std::logic_error, "Missing or not object: \"" << pointerPath << "\"") \
+  } \
+  value = pv; \
+}
+
+#define GET_JSON_AS_ARRAY(jsonVal, pointerPath, value) \
+{ \
+  const rapidjson::Value *pv = rapidjson::Pointer(pointerPath).Get(jsonVal); \
+  if (!(pv && pv->IsArray())) { \
+    THROW_EXC_TRC_WAR(std::logic_error, "Missing or not array: \"" << pointerPath << "\"") \
+  } \
+  value = pv; \
+}
+
+//#define VERIFY_JSON_TYPE(jsonVal, jsonType) \
+//{ \
+//  if (!(jsonVal->Is##jsonType())) { \
+//    THROW_EXC_TRC_WAR(std::logic_error, "Missing or not array: \"" << pointerPath << "\"") \
+//  } \
+//  value = pv; \
+//}
