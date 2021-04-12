431b7ca3438d8adfb1f18da7cb777d34c05fb7fc
Frantisek Mikulu <frantisek.mikulu@logimic.com>
2021-04-12 11:02:24 +0200

    more generic tokenizer

--- include/mqtt_utils.h
+++ include/mqtt_utils.h
@@ -3,13 +3,13 @@
 #include <vector>
 
 namespace shape {
-  std::vector<std::string> tokenizeTopic(const std::string & tokens)
+  std::vector<std::string> tokenizeTopic(const std::string & tokens, char delimit = '/')
   {
     std::vector<std::string> retval;
     std::string s = tokens;
     size_t pos = 0;
 
-    while ((pos = s.find('/')) != std::string::npos) {
+    while ((pos = s.find(delimit)) != std::string::npos) {
       retval.push_back(s.substr(0, pos));
       s.erase(0, pos + 1);
     }
